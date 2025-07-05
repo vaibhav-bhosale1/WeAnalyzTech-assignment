1c5e856a5de36dd9184de6babc5353e4e2de2a80
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-07-05 13:01:31 +0530

    Minor fixes in contexts

--- components/OperationLayout.tsx
+++ components/OperationLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../src/contexts/AuthContext';
 import { 
   LayoutDashboard, 
   CheckCircle, 
